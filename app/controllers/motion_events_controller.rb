dca7f6c045496c64ac0b53076244ceab8813f7fb
Eric Desjardins <eric.desjardins11@gmail.com>
2017-06-14 20:04:28 -0300

    Skip token validation

--- app/controllers/motion_events_controller.rb
+++ app/controllers/motion_events_controller.rb
@@ -1,4 +1,5 @@
 class MotionEventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_motion_event, only: [:show, :edit, :update, :destroy]
 
   # GET /motion_events
